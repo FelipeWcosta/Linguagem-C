743f59722151878b6dd566548f265c0c1dcbf628
FelipeWcosta <costaf138@gmail.com>
2022-04-16 19:34:50 -0300

    16/04/2022

---
+++ script003.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int MinhaIdade = 22;
+    int PaiIdade = 49;
+    int MaeIdade = 38;
+    int IdadeTotal;
+    IdadeTotal = MinhaIdade + PaiIdade + MaeIdade;
+    printf("Minha idade eh %i.\nIdade do meu pai eh %i.\nIdade da minha mae eh %i.\nA idade total eh %i.\n", MinhaIdade, PaiIdade, MaeIdade, IdadeTotal);
+    return 0;
+}
